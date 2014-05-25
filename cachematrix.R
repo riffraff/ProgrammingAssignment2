3e1012d7800a25e818a2905bd74b203d3867fb35
Gabriele Renzi <rff.rff@gmail.com>
2014-05-25 12:29:52 +0200

    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A "matrix" that carries an embedded cached value of it's inverse
 
-## Write a short comment describing this function
+# the matrix is implemented as a "poor man's object" i.e. a record of
+# closures with a shared mutable environment. 
+# the two functions set() and get() allow accessing and mutating the 
+# embedded matrix object, while set.inverse and get.inverse allow 
+# manipulation of the inverse object in the shared environment.
+#
+#
+
 
 makeCacheMatrix <- function(x = matrix()) {
-  cached.result <- NULL
+  inverse <- NULL
   set <- function(y) {
     x <<- y
-    cached.result <<- NULL
+    inverse <<- NULL
   }
   get <- function() x
-  set.cached.result <- function(result) cached.result <<- result
-  get.cached.result <- function() cached.result
+  set.inverse <- function(result) inverse <<- result
+  get.inverse <- function() inverse
   list(set = set, get = get,
-       set.cached.result = set.cached.result,
-       get.cached.result = get.cached.result)
+       set.inverse = set.inverse,
+       get.inverse = get.inverse)
 }
 
 
-## Write a short comment describing this function
+# Cache solve can be used to get the inverse of a matrix 
+# in a smart way by looking up the cached value and memoizing 
+# it after a computation for future use. 
 
 cacheSolve <- function(matrix, ...) {
-  res <- matrix$get.cached.result()
+  res <- matrix$get.inverse()
   if(!is.null(res)) {
     message("getting cached data")
     return(res)
   }
   data <- matrix$get()
   res <- solve(data)
-  matrix$set.cached.result(res)
+  matrix$set.inverse(res)
   res
 }
 

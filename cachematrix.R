752150be5347d934da17ee3a09acf4aba49923e8
Gabriele Renzi <rff.rff@gmail.com>
2014-05-25 12:24:44 +0200

    implement cache solve and cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cached.result <- NULL
+  set <- function(y) {
+    x <<- y
+    cached.result <<- NULL
+  }
+  get <- function() x
+  set.cached.result <- function(result) cached.result <<- result
+  get.cached.result <- function() cached.result
+  list(set = set, get = get,
+       set.cached.result = set.cached.result,
+       get.cached.result = get.cached.result)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+  res <- matrix$get.cached.result()
+  if(!is.null(res)) {
+    message("getting cached data")
+    return(res)
+  }
+  data <- matrix$get()
+  res <- solve(data)
+  matrix$set.cached.result(res)
+  res
 }
+
+#example of invertible matrix
+# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# h8 <- hilbert(8)
